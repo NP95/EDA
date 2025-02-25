d180afb07f23ba7b7fe3b3cdb31f6c7ab68e42e2
Nishant Pani <nishantpani95@gmail.com>
2025-02-24 20:48:36 -0800

    Made corrections

--- STA/parser.cpp
+++ STA/parser.cpp
@@ -1,7 +1,6 @@
+// parser.hpp
 #ifndef PARSER_HPP
 #define PARSER_HPP
-
-//Header Guards
 
 #include <string>
 #include <vector>
@@ -9,45 +8,64 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
-#include <functional>
-#include <optional>
 #include <memory>
 
-//Forward Declarations
+// Forward declarations
 class Circuit;
 class Library;
 
-//A General Parser Class that can be specialized for different file types
-class Parser
-{
-    protected:
+// Base Parser class
+class Parser {
+protected:
     std::string filename_;
     std::ifstream file_;
     
-    //Helper methods for parsing
+    // Helper methods
     bool openFile();
     std::string getLine();
-    std::vector<std::string> tokenize(const std::string& line, char delimiter =' ');
+    std::vector<std::string> tokenize(const std::string& line, char delimiter = ' ');
 
-    public:
+public:
     explicit Parser(const std::string& filename) : filename_(filename) {}
-    virtual ~Parser() { if (file_.is_open()) file_.close();}
-
-    //Pure Virtual Function that derived classes must implement
+    virtual ~Parser() { if (file_.is_open()) file_.close(); }
+    
+    // Pure virtual function
     virtual bool parse() = 0;
 };
 
-class LibertyParser : public Parser {
-    private:
-    Library& library_;
+// NetlistParser declaration
+class NetlistParser : public Parser {
+private:
+    Circuit& circuit_;
+    
+    // Private parsing functions
+    bool parseInputs(const std::string& line);
+    bool parseOutputs(const std::string& line);
+    bool parseGate(const std::string& line);
+    bool parseDFF(const std::string& line);
 
-    //Private Parsing functions for different sections
-    bool parseDelayTable(const std::string& gate_type);
-    bool parseSlowTable(const std::string& gate_type);
-    bool parseDelayTable(const std::string& line);
-
+public:
+    NetlistParser(const std::string& filename, Circuit& circuit) 
+        : Parser(filename), circuit_(circuit) {}
+    
     bool parse() override;
 };
 
+// LibertyParser declaration
+class LibertyParser : public Parser {
+private:
+    Library& library_;
+    
+    // Private parsing functions
+    bool parseDelayTable(const std::string& gate_type);
+    bool parseSlewTable(const std::string& gate_type);
+    bool parseCapacitance(const std::string& line);
 
-#endif //PARSER_HPP
+public:
+    LibertyParser(const std::string& filename, Library& library) 
+        : Parser(filename), library_(library) {}
+    
+    bool parse() override;
+};
+
+#endif // PARSER_HPP
