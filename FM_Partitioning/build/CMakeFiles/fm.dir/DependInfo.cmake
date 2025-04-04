27f4ed9666cdeca25245ae157a392925b2296381
Nishant Pani <nishantpani95@gmail.com>
2025-04-04 16:32:08 -0700

    Add debug output to track segmentation fault in FM algorithm - Add detailed debug output in FMEngine constructor and initialization - Add debug output in run() to track pass progress - Add extensive debug output in runPass() to track cell moves - Track progress of partition initialization, gain calculations, and move operations - Last known point before segfault: Starting first pass after initialization

---
+++ FM_Partitioning/build/CMakeFiles/fm.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nishant/EDA/FM_Partitioning/Algorithm/FMEngine.cpp" "CMakeFiles/fm.dir/Algorithm/FMEngine.cpp.o" "gcc" "CMakeFiles/fm.dir/Algorithm/FMEngine.cpp.o.d"
+  "/home/nishant/EDA/FM_Partitioning/DataStructures/GainBucket.cpp" "CMakeFiles/fm.dir/DataStructures/GainBucket.cpp.o" "gcc" "CMakeFiles/fm.dir/DataStructures/GainBucket.cpp.o.d"
+  "/home/nishant/EDA/FM_Partitioning/DataStructures/Netlist.cpp" "CMakeFiles/fm.dir/DataStructures/Netlist.cpp.o" "gcc" "CMakeFiles/fm.dir/DataStructures/Netlist.cpp.o.d"
+  "/home/nishant/EDA/FM_Partitioning/DataStructures/PartitionState.cpp" "CMakeFiles/fm.dir/DataStructures/PartitionState.cpp.o" "gcc" "CMakeFiles/fm.dir/DataStructures/PartitionState.cpp.o.d"
+  "/home/nishant/EDA/FM_Partitioning/IO/OutputGenerator.cpp" "CMakeFiles/fm.dir/IO/OutputGenerator.cpp.o" "gcc" "CMakeFiles/fm.dir/IO/OutputGenerator.cpp.o.d"
+  "/home/nishant/EDA/FM_Partitioning/IO/Parser.cpp" "CMakeFiles/fm.dir/IO/Parser.cpp.o" "gcc" "CMakeFiles/fm.dir/IO/Parser.cpp.o.d"
+  "/home/nishant/EDA/FM_Partitioning/main.cpp" "CMakeFiles/fm.dir/main.cpp.o" "gcc" "CMakeFiles/fm.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
