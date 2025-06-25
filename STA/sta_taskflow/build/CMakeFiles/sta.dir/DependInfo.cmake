2da26b768faae9318c8c94c9b35a8bb8777d719c
Nishant Pani <nishantpani95@gmail.com>
2025-06-25 19:32:09 +0530

    feat: Achieve near-correct results for c-series benchmarks

---
+++ STA/sta_taskflow/build/CMakeFiles/sta.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/nishant/EDA/STA/sta_taskflow/src/Circuit.cpp" "CMakeFiles/sta.dir/src/Circuit.cpp.o" "gcc" "CMakeFiles/sta.dir/src/Circuit.cpp.o.d"
+  "/home/nishant/EDA/STA/sta_taskflow/src/CircuitNode.cpp" "CMakeFiles/sta.dir/src/CircuitNode.cpp.o" "gcc" "CMakeFiles/sta.dir/src/CircuitNode.cpp.o.d"
+  "/home/nishant/EDA/STA/sta_taskflow/src/GateDatabase.cpp" "CMakeFiles/sta.dir/src/GateDatabase.cpp.o" "gcc" "CMakeFiles/sta.dir/src/GateDatabase.cpp.o.d"
+  "/home/nishant/EDA/STA/sta_taskflow/src/Logger.cpp" "CMakeFiles/sta.dir/src/Logger.cpp.o" "gcc" "CMakeFiles/sta.dir/src/Logger.cpp.o.d"
+  "/home/nishant/EDA/STA/sta_taskflow/src/TaskflowTimingEngine.cpp" "CMakeFiles/sta.dir/src/TaskflowTimingEngine.cpp.o" "gcc" "CMakeFiles/sta.dir/src/TaskflowTimingEngine.cpp.o.d"
+  "/home/nishant/EDA/STA/sta_taskflow/src/TimingUtils.cpp" "CMakeFiles/sta.dir/src/TimingUtils.cpp.o" "gcc" "CMakeFiles/sta.dir/src/TimingUtils.cpp.o.d"
+  "/home/nishant/EDA/STA/sta_taskflow/src/main.cpp" "CMakeFiles/sta.dir/src/main.cpp.o" "gcc" "CMakeFiles/sta.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ STA/sta_taskflow/build/CMakeFiles/sta.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sta.dir/src/Circuit.cpp.o"
+  "CMakeFiles/sta.dir/src/Circuit.cpp.o.d"
+  "CMakeFiles/sta.dir/src/CircuitNode.cpp.o"
+  "CMakeFiles/sta.dir/src/CircuitNode.cpp.o.d"
+  "CMakeFiles/sta.dir/src/GateDatabase.cpp.o"
+  "CMakeFiles/sta.dir/src/GateDatabase.cpp.o.d"
+  "CMakeFiles/sta.dir/src/Logger.cpp.o"
+  "CMakeFiles/sta.dir/src/Logger.cpp.o.d"
+  "CMakeFiles/sta.dir/src/TaskflowTimingEngine.cpp.o"
+  "CMakeFiles/sta.dir/src/TaskflowTimingEngine.cpp.o.d"
+  "CMakeFiles/sta.dir/src/TimingUtils.cpp.o"
+  "CMakeFiles/sta.dir/src/TimingUtils.cpp.o.d"
+  "CMakeFiles/sta.dir/src/main.cpp.o"
+  "CMakeFiles/sta.dir/src/main.cpp.o.d"
+  "sta"
+  "sta.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sta.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
